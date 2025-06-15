b0314e595f359f44a014afd1f051f896cee4db50
Nika-Chinchaladze <nika2019chinchaladze@gmail.com>
2025-06-15 17:16:04 +0400

    allure reporter integrated

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,8 +1,12 @@
 import pytest
+import allure
 from tests.base_test import BaseTest
 
 @pytest.mark.regression
 class TestHomePage(BaseTest):
+    """Class responsible for HomePage testing"""
+    @allure.title('Test Home Page')
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_home_page(self):
         self.home_page.actions.navigate_to_url(self.home_page.URL)
         assert self.home_page.assertions.verify_element_is_displayed(self.home_page.title)
